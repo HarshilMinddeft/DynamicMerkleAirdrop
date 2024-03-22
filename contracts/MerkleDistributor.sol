6fbe1bd4ec04cbac1ce7219149dcfdc7103e0bb5
HarshilMinddeft <harshil.mandali@minddeft.net>
2024-03-22 18:56:32 +0530

    second commite

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -1,51 +1,64 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.0;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleDistributor {
     using SafeERC20 for IERC20;
+
     address public immutable token;
     bytes32 public immutable merkleRoot;
-    uint256 public dropAmount;
+    uint256 public dropAmont;
 
     mapping(address => uint) private addressesClaimed;
+    // mapping to store token amounts for each address
+    mapping(address => uint256) private addressTokenAmount;
+
     // This is a packed array of booleans.
     mapping(uint256 => uint256) private claimedBitMap;
-    event Claimed(address indexed _from, uint256 _dropAmount);
-    constructor(address token_, bytes32 merkleRoot_, uint256 dropAmount_) {
+    event Claimed(address indexed _from, uint _dropAmount);
+
+    constructor(
+        address token_,
+        bytes32 merkleRoot_,
+        uint256 dropAmont_,
+        address[] memory recipients,
+        uint256[] memory amounts
+    ) {
+        require(recipients.length == amounts.length, "Lengths do not match");
         token = token_;
         merkleRoot = merkleRoot_;
-        dropAmount = dropAmount_;
+        dropAmont = dropAmont_;
+
+        // Set token amounts for each address
+        for (uint256 i = 0; i < recipients.length; i++) {
+            addressTokenAmount[recipients[i]] = amounts[i];
+        }
     }
+
     function claim(bytes32[] calldata merkleProof) external {
         require(
             addressesClaimed[msg.sender] == 0,
-            "MerkleDistributor: Drop already claimed"
+            "MerkleDistributor:Drop is already calimed"
         );
-        // Verify merkle proof
+        // to verify merkle proof
         bytes32 node = keccak256(abi.encodePacked(msg.sender));
         require(
             MerkleProof.verify(merkleProof, merkleRoot, node),
-            "MerkleDistributor: Invalid proof"
+            "Invalid proof"
         );
-        // Generate random drop amount
-        uint256 randomSeed = uint256(
-            keccak256(
-                abi.encodePacked(
-                    block.timestamp,
-                    blockhash(block.number - 1),
-                    msg.sender
-                )
-            )
-        );
-        // Add 1 to avoid getting zero
-        uint256 randomAmount = (randomSeed % dropAmount) + 1;
-        // Mark it claimed
+        // Mark it claimed and send the token.
         addressesClaimed[msg.sender] = 1;
-        // Send the token
-        IERC20(token).safeTransfer(msg.sender, randomAmount);
-        emit Claimed(msg.sender, randomAmount);
+        uint256 amountToClaim = addressTokenAmount[msg.sender]; // Retrieve the token amount for the claimer
+        require(
+            amountToClaim > 0, // Ensure the claimer has a valid token amount set
+            "Token amount not set for the claimer"
+        );
+        require(
+            IERC20(token).transfer(msg.sender, amountToClaim),
+            "Transfer failed"
+        );
+        emit Claimed(msg.sender, amountToClaim);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,18 +5,25 @@ const fs = require("fs").promises;
 
 async function main() {
   [signer1, signer2] = await ethers.getSigners();
-
   const customAddress = "0x4e25aec97De40E34F503bf123fCad04Afe02046f";
   signer2 = { address: customAddress };
 
-  // signer3 ={address :"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"}
+  const customAddress3 = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+  signer3 = { address: customAddress3 };
 
-  walletAddresses = [signer1, signer2].map((s) => s.address);
+// signer3 ={address :"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"}
+
+  walletAddresses = [signer1, signer2, signer3].map((s) => s.address);
 
   leaves = walletAddresses.map((x) => KECCAK256(x));
   tree = new MerkleTree(leaves, KECCAK256, { sortPairs: true });
   PuffCoin = await ethers.getContractFactory("PuffCoin", signer1);
   token = await PuffCoin.deploy();
+  const amounts = [
+    BigNumber.from("5000000000000000000"),
+    BigNumber.from("3000000000000000000"),
+    BigNumber.from("6000000000000000000"),
+  ]; // Amounts for each recipient
   MerkleDistributor = await ethers.getContractFactory(
     "MerkleDistributor",
     signer1
@@ -25,12 +32,13 @@ async function main() {
   distributor = await MerkleDistributor.deploy(
     token.address,
     tree.getHexRoot(),
-    BigNumber.from("6000000000000000000")
+    BigNumber.from("20000000000000000000"),
+    walletAddresses,
+    amounts
   );
-
   await token
     .connect(signer1)
-    .mint(distributor.address, BigNumber.from("90000000000000000000"));
+    .mint(distributor.address, BigNumber.from("70000000000000000000"));
 
   console.log("PuffCoin:", token.address);
   console.log("MerkleDistributor:", distributor.address);
